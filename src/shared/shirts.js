376fc459579b6f63856110fe9855e84518601363
yingying0906 <popo090699@gmail.com>
2023-04-23 16:36:23 +0800

    Upload Hw4
    I keep pushing the wrong repo

--- src/shared/shirts.js
+++ src/shared/shirts.js
@@ -31,96 +31,124 @@ import DefaultMaleBack from "../assets/shirt_images/default-m-back.png";
 import DefaultFemaleFront from "../assets/shirt_images/default-w-front.png";
 import DefaultFemaleBack from "../assets/shirt_images/default-w-back.png";
 
+const shirtArr = [
+	{
+		name: "Beep Boop",
+		description:
+			"Once upon a time, a mighty guide guarded the intersection of Forbes and Morewood, and would dutifully direct distracted college students when it was safe to cross the street. Its voice was soothing, strong, and steady. Its name was beep boop.",
+		price: "$19.99",
+		colors: {
+			white: {
+				front: BeepBoopWhiteFront,
+				back: BeepBoopWhiteBack,
+			},
+			blue: {
+				front: BeepBoopBlueFront,
+				back: BeepBoopBlueBack,
+			},
+			pink: {
+				front: BeepBoopPinkFront,
+				back: BeepBoopPinkBack,
+			},
+			red: {
+				front: BeepBoopRedFront,
+				back: BeepBoopRedBack,
+			},
+		},
+		default: {
+			front: DefaultMaleFront,
+			back: DefaultMaleBack,
+		},
+	},
+	{
+		name: "Car-negie Mellon",
+		description:
+			"As you move in to campus, one of the first memories so many students have is driving up to their dorm, unloading their bags, and moving in. How do they arrive to campus? By car, of course!",
+		price: "$10.99",
+		colors: {
+			white: {
+				front: CarWhiteFront,
+				back: CarWhiteBack,
+			},
+			blue: {
+				front: CarBlueFront,
+				back: CarBlueBack,
+			},
+			green: {
+				front: CarGreenFront,
+				back: CarGreenBack,
+			},
+			yellow: {
+				front: CarYellowFront,
+				back: CarYellowBack,
+			},
+			red: {
+				front: CarRedFront,
+				back: CarRedBack,
+			},
+		},
+		default: {
+			front: DefaultFemaleFront,
+			back: DefaultFemaleBack,
+		},
+	},
+	{
+		name: "Forever Plaid",
+		price: "$13.99",
+		description:
+			"Proudly wear your tartan plaid as a patch on your front shirt pocket. And on the back, ask the important question that all CMU students should know the answer to: got plaid?",
+		colors: {
+			white: {
+				front: PlaidWhiteFront,
+				back: PlaidWhiteBack,
+			},
+			pink: {
+				front: PlaidPinkFront,
+				back: PlaidPinkBack,
+			},
+		},
+		default: {
+			front: DefaultFemaleFront,
+			back: DefaultFemaleBack,
+		},
+	},
+	{
+		name: "Carnegie Mellon Melon",
+		description:
+			"Andrew Mellon? Or Watermelon? We may never truly know who the true Mellon is, but this shirt definitely has an opinion. Add some summer fruit to your school spirt!",
+		price: "$6.99",
+		colors: {
+			white: {
+				front: MelonWhiteFront,
+				back: MelonWhiteBack,
+			},
+		},
+		default: {
+			front: DefaultMaleFront,
+			back: DefaultMaleBack,
+		},
+	},
+	{
+		name: "Own Shirt",
+		description: "Balalabalala",
+		price: null,
+		colors: {
+			white: {
+				back: DefaultMaleBack,
+			},
+			azul: {
+				front: DefaultMaleFront,
+			},
+			purple: {
+				front: DefaultMaleFront,
+				back: DefaultMaleBack,
+			},
+		},
+		default: {
+			front: DefaultMaleFront,
+			back: DefaultMaleBack,
+		},
+	},
+];
 
-export default [{
-    "name": "Beep Boop",
-    "description": "Once upon a time, a mighty guide guarded the intersection of Forbes and Morewood, and would dutifully direct distracted college students when it was safe to cross the street. Its voice was soothing, strong, and steady. Its name was beep boop.",
-    "price": "$19.99",
-    "colors": {
-        "white": {
-            "front": BeepBoopWhiteFront,
-            "back": BeepBoopWhiteBack
-        },
-        "blue": {
-            "front": BeepBoopBlueFront,
-            "back": BeepBoopBlueBack
-        },
-        "pink": {
-            "front": BeepBoopPinkFront,
-            "back": BeepBoopPinkBack
-        },
-        "red": {
-            "front": BeepBoopRedFront,
-            "back": BeepBoopRedBack
-        }
-    },
-    "default": {
-        "front": DefaultMaleFront,
-        "back": DefaultMaleBack
-    }
-},
-{
-    "name": "Car-negie Mellon",
-    "description": "As you move in to campus, one of the first memories so many students have is driving up to their dorm, unloading their bags, and moving in. How do they arrive to campus? By car, of course!",
-    "price": "$10.99",
-    "colors": {
-        "white": {
-            "front": CarWhiteFront,
-            "back": CarWhiteBack
-        },
-        "blue": {
-            "front": CarBlueFront,
-            "back": CarBlueBack
-        },
-        "green": {
-            "front": CarGreenFront,
-            "back": CarGreenBack
-        },
-        "yellow": {
-            "front": CarYellowFront,
-            "back": CarYellowBack
-        },
-        "red": {
-            "front": CarRedFront,
-            "back": CarRedBack
-        }
-    },
-    "default": {
-        "front": DefaultFemaleFront,
-        "back": DefaultFemaleBack
-    }
-},
-{
-    "name": "Forever Plaid",
-    "price": "$13.99",
-    "description": "Proudly wear your tartan plaid as a patch on your front shirt pocket. And on the back, ask the important question that all CMU students should know the answer to: got plaid?",
-    "colors": {
-        "white": {
-            "front": PlaidWhiteFront,
-            "back": PlaidWhiteBack
-        },
-        "pink": {
-            "front": PlaidPinkFront,
-            "back": PlaidPinkBack
-        }
-    },
-    "default": {
-        "front": DefaultFemaleFront,
-        "back": DefaultFemaleBack
-    }
-},
-{
-    "name": "Carnegie Mellon Melon",
-    "description": "Andrew Mellon? Or Watermelon? We may never truly know who the true Mellon is, but this shirt definitely has an opinion. Add some summer fruit to your school spirt!",
-    "price": "$6.99",
-    "colors": {
-        "white": {
-            "front": MelonWhiteFront,
-            "back": MelonWhiteBack
-        }
-    },
-    "default": {
-        "front": DefaultMaleFront,
-        "back": DefaultMaleBack
-    }
-}];
+export default shirtArr;
